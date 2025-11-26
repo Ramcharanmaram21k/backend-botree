5fa561230f2c4940d8630da3fc99df1539f83b3d
Ramcharan <166367457+Ramcharanmaram21k@users.noreply.github.com>
2025-11-26 22:17:57 +0530

    updated for localhost again

--- database.py
+++ database.py
@@ -1,13 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-# --- VERCEL FIX: DATABASE IN TMP ---
-SQLALCHEMY_DATABASE_URL = "sqlite:////tmp/sql_app.db"
+DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/grievance_system"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
